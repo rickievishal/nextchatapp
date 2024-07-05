a52d16920c7c7ba3faeb5c5c38737134ac6a9932
Vishal Thangamaiappan <rickievishalyt@gmail.com>
2024-07-05 19:31:07 +0530

    integrated pass in chatlist

--- src/app/chatroom/chatroomlist/page.tsx
+++ src/app/chatroom/chatroomlist/page.tsx
@@ -7,8 +7,9 @@ import { Tooltip } from "@nextui-org/tooltip";
 import { Button } from "@nextui-org/react"
 import { GoLock } from 'react-icons/go';
 import { IoCloseSharp } from "react-icons/io5";
-
+import Link from 'next/link'
 import Image from 'next/image';
+
 const page = () => {
     const [data, setdata] = useState([])
     const getchatrooms = async () => {
@@ -24,7 +25,9 @@ const page = () => {
         getchatrooms()
     }, [])
     const [ischatroomclicked, setIschatroomclicked] = useState(false)
-
+    const gotochatroom = (path :string) =>{
+        console.log(path)
+    }
 
     return (
         <>
@@ -32,9 +35,9 @@ const page = () => {
                 {
                     ischatroomclicked && (<div className='absolute top-0 right-0 z-40 w-full h-full opacity-50 backdrop-blur-md flex justify-center items-center'>
                         <div className='relative w-[500px] h-[600px] bg-[#160d08] border-[#F2613F] border rounded-lg flex flex-col justify-start items-center'>
-                            <IoCloseSharp className='text-[#F2613F] text-4xl absolute top-3 right-3' onClick={()=>{
+                            <IoCloseSharp className='text-[#F2613F] text-4xl absolute top-3 right-3' onClick={() => {
                                 setIschatroomclicked(false)
-                            }}/>
+                            }} />
                             <p className='pt-[100px] w-[150px] text-center leading-tight'>You know what I want. The password. Now!!</p>
                             <input className='mt-2 w-[150px]  border-[#F2613F] border text-[#F2613F] active:outline-none focus:outline-none px-3' type="password" />
                             <Image className='absolute bottom-0' src={`/frog.png`} alt={""} width="400" height="400" />
@@ -53,14 +56,17 @@ const page = () => {
                         <div className='h-auto overflow-y-auto my-3 selection:bg-[#0C0C0C]'>
                             {
                                 data.map((data) => (
-                                    <div className='w-full group hover:bg-[#F2613F] flex justify-between items-center cursor-pointer' onClick={()=>{
-                                        setIschatroomclicked(true)
+                                    
+                                    <div className='w-full group hover:bg-[#F2613F] flex justify-between items-center cursor-pointer' onClick={() => {
                                     }}>
                                         <div className='w-full  px-4 group-hover:text-[#0C0C0C] text-[#F2613F] py-2'>{data.chatroomid}</div>
                                         <div className='events-buttons'>
-                                            <Tooltip content="Get in??" className='bg-[#0C0C0C] text-[#ef7154] border border-[#ef7154] px-2 text-sm rounded-lg overflow-hidden mb-2'>
-                                                <button className='hover:underline group-hover:text-[#0C0C0C] text-[#ef7154]'>Join</button>
-                                            </Tooltip>
+                                        <Link href={"http://localhost:3000/chatroom/"+data.chatid}>
+                                                <Tooltip content="Get in??" className='bg-[#0C0C0C] text-[#ef7154] border border-[#ef7154] px-2 text-sm rounded-lg overflow-hidden mb-2'>
+                                                    <button className='hover:underline group-hover:text-[#0C0C0C] text-[#ef7154]'>Join</button>
+                                                </Tooltip>
+                                        </Link>
+
                                         </div>
                                         <div className='text-[#ef7154] ml-2'>
                                             <Tooltip content="protected mfers" className='bg-[#0C0C0C] text-[#ef7154] border border-[#ef7154] px-2 text-sm rounded-lg overflow-hidden mb-2'>
