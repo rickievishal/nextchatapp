78b5d4995782864cbf366c233be28a37060e0d7b
Vishal Thangamaiappan <rickievishalyt@gmail.com>
2024-07-08 21:56:12 +0530

    optimized for seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,14 @@ import Headerdiv from "./Headerdiv";
 import Link from "next/link";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import Body from "./Body";
+import Head from "next/head";
 
 const inter = IBM_Plex_Mono({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600"] });
 
 export const metadata: Metadata = {
   title: "Victoria'sceret",
-  description: "A dynamic chatroom built on nextjs and firebase",
+  description: "the ultimate chat app for college students! Stay connected with your friends through private chat rooms, even when smartphones and social media are restricted. Enjoy secure, real-time messaging with minimal network access",
+  keywords:"college chat app, private chat rooms, secure messaging, student communication, minimal network access, instagram,omegle"
 };
 
 export default function RootLayout({
@@ -22,16 +24,16 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-
+      
       <body className="w-screen h-[] flex flex-col items-center relative bg-black 
      ">
         <div className=" w-full  h-full flex flex-col justify-center items-center relative z-20  sm:pt-0 " >
           <Body></Body>
           <div className="w-full h-full sm:px-10 lg:py-[50px] lg:px-[100px] xl:px-[200px] lg:px-30 flex lg:grid grid-cols-2 flex-col justify-center items-center  z-20 ">
-            
-            
-              {children}
-              <SpeedInsights />
+
+
+            {children}
+            <SpeedInsights />
 
           </div>
           <div className="w-full text-sm text-gray-400 flex justify-center items-center py-4">Created by Vishal with fking ❤️🫰🏻</div>
